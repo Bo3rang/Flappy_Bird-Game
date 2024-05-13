e6c612f3ea6449d407ae7d92836ae983959b16f7
Dương Tiến Cường <96168511+Bo3rang@users.noreply.github.com>
2024-05-13 17:01:32 +0700

    final

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 import pygame, sys, random
 
-def draw_floor():                                   # Hàm 2 floor
+def draw_floor():                                                           # Hàm 2 floor
     screen.blit(floor, (floor_x_pos, 650))
     screen.blit(floor, (floor_x_pos + 432, 650))
 
@@ -14,7 +14,7 @@ def create_pipe():
 # Hàm di chuyển Pipe
 def move_pipe(pipes):
     for p in pipes:
-        p.centerx -= 5                              # Di chuyển Pipe sang bên trái
+        p.centerx -= 5                                                      # Di chuyển Pipe sang bên trái
     return pipes
 
 # Hàm vẽ Pipe
@@ -30,8 +30,10 @@ def draw_pipe(pipes):
 def check_collision(pipes):
     for p in pipes:
         if bird_rect.colliderect(p):
+            sound_hit.play()
             return False
     if bird_rect.top <= -75 or bird_rect.bottom >= 650:
+        sound_hit.play()
         return False
     return True
 
@@ -47,23 +49,40 @@ def bird_animation():
     return new_bird, new_bird_rect
 
 # Hàm hiển thị Score
-def score_display():
-    score = game_font.render('Score', True, (255,255,255))
-    score_rect = score.get_rect(center = (288, 100))
-    screen.blit(score, score_rect)
+def score_display(game_state):
+    if game_state == 'Main game':
+        score = game_font.render(str(int(score_game)), True, (255,255,255))
+        score_rect = score.get_rect(center = (216, 100))
+        screen.blit(score, score_rect)
+    if game_state == "Game over":
+        score = game_font.render(f'Score: {int(score_game)}', True, (255,255,255))
+        score_rect = score.get_rect(center = (216, 100))
+        screen.blit(score, score_rect)
 
-pygame.init                                         # Bắt đầu pygame.
-screen =  pygame.display.set_mode((432, 768))       # Hiển thị cửa sổ pygame.
-clock = pygame.time.Clock()                         # Khởi tạo FPS.
-game_font = pygame.font.Font('FileGame/04B_19.TTF', 40)      # Tạo font       
+        hight = game_font.render(f'Hight Score: {int(hight_score)}', True, (255,255,255))
+        hight_rect = score.get_rect(center = (160, 630))
+        screen.blit(hight, hight_rect)
+
+# Hàm Hight_Score
+def update_score(score_game, hight_score):
+    if score_game > hight_score:
+        hight_score = score_game
+    return hight_score
+
+pygame.init                                                                 # Bắt đầu pygame.
+pygame.mixer.pre_init(frequency=44100, size=-16, channels=2, buffer=512)
+screen =  pygame.display.set_mode((432, 768))                               # Hiển thị cửa sổ pygame.
+clock = pygame.time.Clock()                                                 # Khởi tạo FPS.
+game_font = pygame.font.init()
+game_font = pygame.font.Font("FileGame/04B_19.TTF", 40)
 
 # Chèn Background
 bg = pygame.image.load('FileGame/assets/background-night.png').convert()    # .convert dùng để đổi file hình ảnh nhẹ hơn => load nhanh hơn
-bg = pygame.transform.scale2x(bg)                   # X2 background để đầy màn.
+bg = pygame.transform.scale2x(bg)                                           # X2 background để đầy màn.
 # Chèn Floor.
 floor = pygame.image.load('FileGame/assets/floor.png').convert()
 floor = pygame.transform.scale2x(floor)  
-floor_x_pos = 0                                     # Đặt vị trí x ban đầu bằng 0.
+floor_x_pos = 0                                                             # Đặt vị trí x ban đầu bằng 0.
 # Chèn Bird.
 bird_down = pygame.transform.scale2x(pygame.image.load('FileGame/assets/yellowbird-downflap.png')).convert_alpha()
 bird_mid = pygame.transform.scale2x(pygame.image.load('FileGame/assets/yellowbird-midflap.png')).convert_alpha()
@@ -71,9 +90,9 @@ bird_up = pygame.transform.scale2x(pygame.image.load('FileGame/assets/yellowbird
 bird_list = [bird_down, bird_mid, bird_up]          
 bird_index = 0
 bird = bird_list[bird_index]
-bird_rect = bird.get_rect(center = (100, 384))      # Tạo hình chữ nhật xung quanh Bird.
-gravity = 0.25                                      # Tạo ra trọng lực.
-bird_movement = 0                                   # Tạo sự di chuyển, = 0 vì lúc đầu Bird chưa di chuyển.
+bird_rect = bird.get_rect(center = (100, 384))                              # Tạo hình chữ nhật xung quanh Bird.
+gravity = 0.25                                                              # Tạo ra trọng lực.
+bird_movement = 0                                                           # Tạo sự di chuyển, = 0 vì lúc đầu Bird chưa di chuyển.
 # Tạo timmer cho Bird
 bird_flap = pygame.USEREVENT + 1
 pygame.time.set_timer(bird_flap, 200)
@@ -84,12 +103,24 @@ pipe = pygame.transform.scale2x(pipe)
 pipe_list = []
 pipe_height = [200, 300, 400]
 # Tạo SpawnPipe
-spawnpipe = pygame.USEREVENT                        # Làm cho xuất hiện Pipe liên tục 
-pygame.time.set_timer(spawnpipe, 1200)              # Set 1.2s sẽ xuất hiện 
+spawnpipe = pygame.USEREVENT                                                # Làm cho xuất hiện Pipe liên tục 
+pygame.time.set_timer(spawnpipe, 1200)                                      # Set 1.2s sẽ xuất hiện 
 # Biến game
 game_active = True
-score = 0
+score_game = 0
 hight_score = 0
+
+# Tạo màn hình kết thúc game
+game_over = pygame.transform.scale2x(pygame.image.load('FileGame/assets/message.png')).convert_alpha()
+game_over_rect = game_over.get_rect(center = (216,384))
+
+# Chèn Sound
+sound_flap = pygame.mixer.init()
+sound_flap = pygame.mixer.Sound('FileGame/sound/sfx_wing.wav')
+sound_hit = pygame.mixer.Sound('FileGame/sound/sfx_hit.wav')
+sound_die = pygame.mixer.Sound('FileGame/sound/sfx_die.wav')
+sound_score = pygame.mixer.Sound('FileGame/sound/sfx_point.wav')
+
 
 # While loop của trò chơi
 while True:
@@ -104,12 +135,14 @@ while True:
             if event.key == pygame.K_SPACE and game_active:
                 # Di chuyển Bird lên trên.
                 bird_movement = 0
-                bird_movement = -11
+                bird_movement = -10
+                sound_flap.play()
             if event.key == pygame.K_SPACE and game_active == False:
                 game_active = True
                 pipe_list.clear()
                 bird_rect.center = (100, 384)
-                bird_movement = -11
+                bird_movement = -10
+                score_game = 0
 
         # Sự kiện xuất hiện Pipe
         if event.type == spawnpipe:
@@ -128,24 +161,30 @@ while True:
 
     if game_active:
         # Hiển thị Pipe
-        pipe_list = move_pipe(pipe_list)                # Lấy tất cả ống trong Pipe list trả về xong di chyển Pipe mới
+        pipe_list = move_pipe(pipe_list)                                    # Lấy tất cả ống trong Pipe list trả về xong di chyển Pipe mới
         draw_pipe(pipe_list)
         game_active = check_collision(pipe_list)
         
         # Hiển thị Bird
         rotate_birded = rotate_bird(bird)
         screen.blit(rotate_birded, bird_rect)
-        bird_movement += gravity                        # Bird càng di chuyển thì Gravity càng tăng
-        bird_rect.centery += bird_movement              # Di chuyển Bird và Rect đi xuống
+        bird_movement += gravity                                            # Bird càng di chuyển thì Gravity càng tăng
+        bird_rect.centery += bird_movement                                  # Di chuyển Bird và Rect đi xuống
+        
+        # Hiển thị Score
+        score_game += 0.01
+        score_display('Main game')
+    else: 
+        hight_score = update_score(score_game, hight_score)
+        score_display('Game over')
+        screen.blit(game_over, game_over_rect)
 
     # Hiển thị Floor
-    floor_x_pos -= 1                                    # Di chuyển Floor lùi về phía sau 
+    floor_x_pos -= 1                                                        # Di chuyển Floor lùi về phía sau 
     draw_floor()  
     # Lặp đi lặp lại floor
     if floor_x_pos <= -432:                         
         floor_x_pos = 0
-
-    score_display()
     
     pygame.display.update()                     
-    clock.tick(120)                                 # FPS là 120.
+    clock.tick(120)                                                         # FPS là 120.
